ea2c611bfba8a48ec0e4cd1d4f9a126f9d65f7ed
ethan fregien <ethanfregien@gmail.com>
2021-11-04 19:40:31 -0500

    actually fixed tests lol

--- app.js
+++ app.js
@@ -10,35 +10,61 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const allEmployees = [];
+const team = [];
 
-console.log("Please build your team");
+
+function employeeInformation() {
+   inquirer.prompt([
+    {
+      type: "list",
+      message: "What type of employee would you like to input",
+      name: "name",
+      choices: ["Manager", "Engineer", "Intern", "Show Summary"],
+    },
+  ]).then(val => {
+    if (val.name === "Manager") {
+      managerInformation();
+    } else if (val.name === "Engineer") {
+      engineerInformation()
+    } else if (val.name === "Intern") {
+      internInformation();
+    } else if (val.name === "Show Summary") {
+      generateHTML(outputPath, render(team));
+    };
+  }); 
+}; 
 
 
 function managerInformation() {
-    return inquirer.prompt([
-      {
-        type: "input",
-        message: "what is your manager's name?",
-        name: "name"
-      },
-      {
-        type: "input",
-        message: "What is your manager's id",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "What is your manager's email?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "What is your manager's office number",
-        name: "number",
-      },
-  ]); 
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "what is your manager's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your manager's id",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your manager's email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your manager's office number",
+      name: "number",
+    },
+  ]).then(function(answer) {
+    let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
+    team.push(manager);
+
+    employeeInformation()
+  })
 }; 
+
 
 
 function engineerInformation() {
@@ -51,7 +77,7 @@ function engineerInformation() {
     {
       type: "input",
       message: "What is your engineer's ID",
-      name: "ID",
+      name: "id",
     },
     {
       type: "input",
@@ -63,8 +89,14 @@ function engineerInformation() {
       message: "What is your engineer's GitHub username",
       name: "GitHub",
     },
-  ]); 
+  ]).then(function(answer) {
+    let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
+    team.push(engineer);
+
+    employeeInformation()
+  })
 }; 
+
 
 
 function internInformation() {
@@ -77,7 +109,7 @@ function internInformation() {
     {
       type: "input",
       message: "What is your intern's ID",
-      name: "ID",
+      name: "id",
     },
     {
       type: "input",
@@ -89,5 +121,23 @@ function internInformation() {
       message: "What is your intern's school",
       name: "school",
     },
-  ]); 
+  ]).then(function(answer) {
+    let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+    team.push(intern);
+
+    employeeInformation()
+  })
 }; 
+
+
+function generateHTML(fileName, data) {
+  fs.writeFile(fileName, data, "utf8", function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("You have successfully written your Employee Summary");
+  });
+};
+
+
+employeeInformation();
